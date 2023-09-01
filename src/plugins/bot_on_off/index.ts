dd06b7647c4fc3d80aab354c917ea9d360bf992f
Lhcfl <lhcfl@outlook.com>
2023-09-01 23:01:21 +0800

    hitokoto， bot status

--- src/plugins/bot_on_off/index.ts
+++ src/plugins/bot_on_off/index.ts
@@ -5,11 +5,14 @@ import { PluginInit } from '../../types/plugin.js';
 
 const bot_off: commandHandleFunction = (app, msg) => {
   db.chat(msg.chat.id).turned_off = true;
-  app.bot?.sendMessage(msg.chat.id, 'bot 已关闭');
+  app.bot?.sendMessage(msg.chat.id, `${app.config?.bot_name}已关闭`);
 }
 const bot_on: commandHandleFunction = (app, msg) => {
   db.chat(msg.chat.id).turned_off = false;
-  app.bot?.sendMessage(msg.chat.id, 'bot 已开机');
+  app.bot?.sendMessage(msg.chat.id, `${app.config?.bot_name}已开机`);
+}
+const bot_status: commandHandleFunction = (app, msg) => {
+  app.bot?.sendMessage(msg.chat.id, db.chat(msg.chat.id).turned_off ? `${app.config?.bot_name}关机中` : `${app.config?.bot_name}开机中`);
 }
 
 const init: PluginInit = (app) => {
@@ -27,6 +30,13 @@ const init: PluginInit = (app) => {
     chat_type: 'all',
     handle: bot_on,
     command: 'bot_on',
+    off_mode: true,
+  });
+  app.registCommand({
+    description: '查询bot当前开关机状态',
+    chat_type: 'all',
+    handle: bot_status,
+    command: 'bot_status',
     off_mode: true,
   })
 }
---
+++ src/plugins/hitokoto/index.ts
@@ -0,0 +1,32 @@
+import { PluginInit } from "@/types/plugin.js";
+
+const api_addr = "https://v1.hitokoto.cn/?c="; //网址
+
+async function get_hitokoto(arg = "") {
+  try {
+    let res = await fetch(api_addr + arg);
+    return (await res.json());
+  } catch (error) {
+    return ({hitokoto: "网络错误"});
+  }
+}
+
+const init: PluginInit = (app) => {
+  app.registCommand({
+    chat_type: 'all',
+    command: 'hitokoto',
+    handle: (App, msg, msgTxt) => {
+      if (!msgTxt) msgTxt = '';
+      const arg = msgTxt.split(' ').join('&c=');
+      get_hitokoto(arg).then(res => {
+        console.log(res);
+        App.bot?.sendMessage(msg.chat.id, `${res.hitokoto} ${res.from ? '——' + res.from : ''}`, {
+          reply_to_message_id: msg.message_id,
+        });
+      });
+    },
+    description: "一言"
+  })
+}
+
+export { init };
