d6835e091b61b16f8bf05ccac06b1306380d278e
Virginia Senioria <91khr@users.noreply.github.com>
2023-09-04 15:09:14 +0800

    feat waife: render the graph with dot

--- src/plugins/waife/index.ts
+++ src/plugins/waife/index.ts
@@ -1,10 +1,16 @@
 import { App } from '@/types/app.js';
 import { PluginInit } from '@/types/plugin.js';
 import { Message, User } from 'node-telegram-bot-api';
+import { tmpdir } from 'os';
+import fs from 'fs';
+import { spawn } from 'child_process';
 
 function getName(user: User): string {
-  let username:string = user.first_name ?
-    user.first_name : (user.username ? user.username : '');
+  let username: string = user.first_name
+    ? user.first_name
+    : user.username
+      ? user.username
+      : '';
   if (user.last_name) {
     username += ' ' + user.last_name;
   }
@@ -12,14 +18,15 @@ function getName(user: User): string {
 }
 
 function htmlify(str: string | undefined): string {
-  if (!str) {str = '';}
+  if (!str) {
+    str = '';
+  }
   str = str.replaceAll('&', '&amp;');
   str = str.replaceAll('<', '&lt;');
   str = str.replaceAll('>', '&gt;');
   str = str.replaceAll('"', '&quot;');
   return str;
 }
-
 
 function initialize_waifes(app: App, msg: Message) {
   if (!app.db.chat(msg.chat.id).waifes) {
@@ -48,13 +55,15 @@ function initialize_waifes(app: App, msg: Message) {
     /**
      * iduserMap[a] 大家上次抽老婆的日期
      */
-    app.db.chat(msg.chat.id).lastwaifedate = (new Date).toDateString();
+    app.db.chat(msg.chat.id).lastwaifedate = new Date().toDateString();
   }
 }
 
 async function add_to_wife(app: App, msg: Message, uid?: number) {
   initialize_waifes(app, msg);
-  if (!uid) { return; }
+  if (!uid) {
+    return;
+  }
   if (app.db.chat(msg.chat.id).waife_ids[uid]) {
     return;
   }
@@ -84,10 +93,12 @@ async function getWaifesList(app: App, msg: Message) {
 }
 
 async function getWaife(app: App, msg: Message) {
-  if (!msg.from?.id) { return; }
-  if ((new Date).toDateString() !== app.db.chat(msg.chat.id).lastwaifedate) {
+  if (!msg.from?.id) {
+    return;
+  }
+  if (new Date().toDateString() !== app.db.chat(msg.chat.id).lastwaifedate) {
     app.db.chat(msg.chat.id).waifemap = {};
-    app.db.chat(msg.chat.id).lastwaifedate = (new Date).toDateString();
+    app.db.chat(msg.chat.id).lastwaifedate = new Date().toDateString();
   }
   if (app.db.chat(msg.chat.id).waifemap[msg.from?.id]) {
     app.bot.sendMessage(msg.chat.id, '你今天已经抽过老婆了哦！', {
@@ -97,9 +108,13 @@ async function getWaife(app: App, msg: Message) {
   }
   const lst = await getWaifesList(app, msg);
   if (lst.length <= 1) {
-    app.bot.sendMessage(msg.chat.id, '抱歉，我还没从群里获取到足够多的人哦，请先回复我一些消息', {
-      reply_to_message_id: msg.message_id,
-    });
+    app.bot.sendMessage(
+      msg.chat.id,
+      '抱歉，我还没从群里获取到足够多的人哦，请先回复我一些消息',
+      {
+        reply_to_message_id: msg.message_id,
+      },
+    );
     return;
   }
   let waife: User = lst[Math.floor(Math.random() * lst.length)];
@@ -109,19 +124,25 @@ async function getWaife(app: App, msg: Message) {
     cnt++;
   }
   if (waife.id === msg.from?.id) {
-    app.bot.sendMessage(msg.chat.id, '获取失败……理论上不会出现这个情况, 请查看bot代码', {
-      reply_to_message_id: msg.message_id,
-    });
+    app.bot.sendMessage(
+      msg.chat.id,
+      '获取失败……理论上不会出现这个情况, 请查看bot代码',
+      {
+        reply_to_message_id: msg.message_id,
+      },
+    );
     return;
   }
   add_to_wife(app, msg, msg.from.id);
   const res = await app.bot.sendMessage(
     msg.chat.id,
-    `${htmlify('获取成功~ 你今天的群友老婆是')} <a href="tg://user?id=${waife.id}">${htmlify(getName(waife))}</a>`,
+    `${htmlify('获取成功~ 你今天的群友老婆是')} <a href="tg://user?id=${
+      waife.id
+    }">${htmlify(getName(waife))}</a>`,
     {
       parse_mode: 'HTML',
       reply_to_message_id: msg.message_id,
-    }
+    },
   );
   app.db.chat(msg.chat.id).waifemap[msg.from?.id] = {
     msg_id: res.message_id,
@@ -130,27 +151,54 @@ async function getWaife(app: App, msg: Message) {
   };
 }
 
+async function renderGraph(id: string, src: string): Promise<Buffer> {
+  const [fname, outname] = [`${tmpdir()}/${id}.gv`, `${tmpdir()}/${id}.png`];
+  await new Promise((res, rej) =>
+    fs.writeFile(fname, src, {}, (err) => {
+      if (err) {
+        rej(err);
+      } else {
+        res({});
+      }
+    }),
+  );
+  await new Promise((res, rej) => {
+    const proc = spawn('dot', [fname, '-Tpng', '-o', outname]);
+    proc.on('close', (code) => {
+      if (code !== 0) {
+        rej(code);
+      } else {
+        res({});
+      }
+    });
+  });
+  return await new Promise((res, rej) =>
+    fs.readFile(outname, {}, (err, data) => {
+      if (err) {
+        rej(err);
+      } else {
+        res(data);
+      }
+    }),
+  );
+}
+
 async function getWaifeGraph(app: App, msg: Message) {
   initialize_waifes(app, msg);
-  if (!msg.from?.id) { return; }
-  const wfMap = app.db.chat(msg.chat.id).waifemap;
-  const iduM = app.db.chat(msg.chat.id).iduserMap;
-  let txt = '```mermaid\n';
-  txt += '\ngraph TD';
-  for (const id in wfMap) {
-    if (wfMap[id]?.waife) {
-      txt += `\n    ${id}["${getName(iduM[id])}"] --> ${wfMap[id]?.waife?.id}["${getName(wfMap[id].waife)}"]`;
-    }
+  if (!msg.from?.id) {
+    return;
   }
-  txt += '\n```';
-  app.bot.sendMessage(
-    msg.chat.id,
-    `老婆图（mermaid）：\n<pre>${htmlify(txt)}</pre>`,
-    {
-      parse_mode: 'HTML',
-      reply_to_message_id: msg.message_id,
-    }
-  );
+  const wfMap = app.db.chat(msg.chat.id).waifemap;
+  const iduser = app.db.chat(msg.chat.id).iduserMap;
+  const getNode = (user: User) => `${user.id}[label="${getName(user)}"]`;
+  const src =
+    'graph G {' +
+    Object.entries<{ waife: User }>(wfMap).map(([id, { waife }]) =>
+      waife ? `  ${getNode(iduser[id])} --> ${getNode(waife)}\n` : '',
+    ) +
+    '}';
+  const rendered = await renderGraph('waife', src);
+  app.bot.sendPhoto(msg.chat.id, rendered);
 }
 
 const init: PluginInit = (app) => {
@@ -158,20 +206,20 @@ const init: PluginInit = (app) => {
     chat_type: ['group', 'supergroup'],
     command: 'waife',
     handle: getWaife,
-    description: '获取老婆！'
+    description: '获取老婆！',
   });
   app.registCommand({
     chat_type: ['group', 'supergroup'],
     command: 'waife_graph',
     handle: getWaifeGraph,
-    description: '获取老婆关系图！'
+    description: '获取老婆关系图！',
   });
   app.registReplyHandle({
     chat_type: ['group', 'supergroup'],
     handle: (appl, msg) => {
       add_to_wife(appl, msg, msg.from?.id);
     },
-    description: '添加老婆！'
+    description: '添加老婆！',
   });
 };
 
