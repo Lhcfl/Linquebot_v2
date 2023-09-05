d57f89faa859335b6146bb57bb25c6af0b382f25
Virginia Senioria <24869594+91khr@users.noreply.github.com>
2023-09-05 16:54:32 +0800

    chore,infra: satisfy CI and tweak lint and prettier

--- src/util/fs.ts
+++ src/util/fs.ts
@@ -2,38 +2,40 @@ import fs from 'fs';
 
 /**
  * 读取文件，如果文件不存在则返回null
- * Default use { encoding: 'utf-8' } as `options`
+ * Default use `{ encoding: 'utf-8' }` as `options`
  * @param fileName filename or file descriptor
- * @param options If the encoding option is specified then this function returns a string. Otherwise it returns a buffer.
+ * @param options Options for reading the file
+ * @returns string if options.encoding is specified, otherwise `Buffer`
  */
-export const readFileIfExists = (
+export function readFileIfExists(
   fileName: string,
-  options:
-    | { encoding: BufferEncoding; flag?: string | undefined }
-    | undefined = { encoding: 'utf-8' },
-) => {
+  options: { encoding: BufferEncoding; flag?: string | undefined } | undefined = {
+    encoding: 'utf-8',
+  }
+) {
   const fileExist = fs.existsSync(fileName);
   if (!fileExist) {
     return null;
   }
   return fs.readFileSync(fileName, options);
-};
+}
 
 const defaultDirs = ['./', '../', './config/', '../config/'];
 
 /**
  * 从里到外读取文件，如果文件不存在则返回null
- * Default use { encoding: 'utf-8' } as `options`
+ * Default use `{ encoding: 'utf-8' }` as `options`
  * @param fileName filename or file descriptor
- * @param options If the encoding option is specified then this function returns a string. Otherwise it returns a buffer.
+ * @param options Options for reading the file
+ * @returns string if options.encoding is specified, otherwise `Buffer`
  */
-export const reverseReadFileIfExists = (
+export function reverseReadFileIfExists(
   fileName: string,
   reverseDirs: string[] = defaultDirs,
-  options:
-    | { encoding: BufferEncoding; flag?: string | undefined }
-    | undefined = { encoding: 'utf-8' },
-) => {
+  options: { encoding: BufferEncoding; flag?: string | undefined } | undefined = {
+    encoding: 'utf-8',
+  }
+) {
   for (const dir of reverseDirs) {
     const fileContent = readFileIfExists(dir + fileName, options);
     if (fileContent !== null) {
@@ -41,4 +43,4 @@ export const reverseReadFileIfExists = (
     }
   }
   return null;
-};
+}
